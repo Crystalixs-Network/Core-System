88710257983561ae58f9269010d5a6a46e6b033a
Merry <svgr1808@outlook.de>
2026-03-02 18:25:03 +0100

    Simplify ObjectMapper configuration

--- common/src/main/java/net/crystalixs/core/common/config/ObjectMapperProvider.java
+++ common/src/main/java/net/crystalixs/core/common/config/ObjectMapperProvider.java
@@ -4,17 +4,14 @@ import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.PropertyNamingStrategies;
 import tools.jackson.databind.SerializationFeature;
 import tools.jackson.databind.json.JsonMapper;
-import tools.jackson.module.kotlin.KotlinFeature;
 import tools.jackson.module.kotlin.KotlinModule;
 
 public final class ObjectMapperProvider {
 
     private static final ObjectMapper MAPPER = JsonMapper.builder()
-            .addModule(new KotlinModule.Builder()
-                    .enable(KotlinFeature.KotlinPropertyNameAsImplicitName)
-                    .build())
+            .addModule(new KotlinModule.Builder().build())
+            .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE) // keyName -> key_name
             .enable(SerializationFeature.INDENT_OUTPUT) // Pretty printing
-            .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
             .build();
 
     private ObjectMapperProvider() {
