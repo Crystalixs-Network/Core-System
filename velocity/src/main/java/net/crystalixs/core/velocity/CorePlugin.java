185ff34c71175fc3c39e273720b5b1806eb3a9f4
Merry <svgr1808@outlook.de>
2026-03-03 00:45:32 +0100

    Add Velocity configuration loader and implement dynamic MOTD support

--- velocity/src/main/java/net/crystalixs/core/velocity/CorePlugin.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/CorePlugin.java
@@ -2,18 +2,58 @@ package net.crystalixs.core.velocity;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyPingEvent;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
+import de.eldoria.jacksonbukkit.JacksonPaper;
 import jakarta.inject.Inject;
+import net.crystalixs.core.common.config.ObjectMapperFactory;
+import net.crystalixs.core.velocity.config.Motd;
+import net.crystalixs.core.velocity.config.VelocityConfig;
+import net.crystalixs.core.velocity.config.VelocityConfigLoader;
+import tools.jackson.databind.ObjectMapper;
 
+import java.nio.file.Path;
 import java.util.logging.Logger;
+
+import static net.kyori.adventure.text.Component.newline;
 
 public class CorePlugin {
 
+    private final ProxyServer server;
+    private final Path dataDirectory;
+    private final Logger logger;
+    private VelocityConfig config;
+
     @Inject
-    private Logger logger;
+    public CorePlugin(ProxyServer server, @DataDirectory Path dataDirectory, Logger logger) {
+        this.server = server;
+        this.dataDirectory = dataDirectory;
+        this.logger = logger;
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        logger.info("Plugin loaded!");
+        ObjectMapper mapper = ObjectMapperFactory.create(builder -> builder.addModule(JacksonPaper.builder().build()));
+        VelocityConfigLoader loader = new VelocityConfigLoader(mapper, logger, dataDirectory.resolve("config.json"));
+        loader.reload();
+
+        config = loader.get();
+
+        server.getEventManager().register(this, this);
+        logger.info("Velocity core plugin has been enabled!");
+    }
+
+    @Subscribe
+    public void onProxyPing(ProxyPingEvent event) {
+        Motd motd = config.motd();
+
+        ServerPing ping = event.getPing().asBuilder()
+                .description(motd.firstLine().append(newline()).append(motd.secondLine()))
+                .build();
+
+        event.setPing(ping);
     }
 
 }
---
+++ velocity/src/main/java/net/crystalixs/core/velocity/config/Motd.java
@@ -0,0 +1,6 @@
+package net.crystalixs.core.velocity.config;
+
+import net.kyori.adventure.text.Component;
+
+public record Motd(Component firstLine, Component secondLine) {
+}
