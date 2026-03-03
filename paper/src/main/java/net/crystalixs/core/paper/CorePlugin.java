f37d552edc97bd765b41c0c6d00dee0aeac1c2b9
Merry <svgr1808@outlook.de>
2026-03-03 19:45:13 +0100

    Fix invalid refactored class names

--- paper/src/main/java/net/crystalixs/core/paper/CorePlugin.java
+++ paper/src/main/java/net/crystalixs/core/paper/CorePlugin.java
@@ -1,8 +1,8 @@
 package net.crystalixs.core.paper;
 
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import net.crystalixs.core.paper.command.PaperAbstractCommandSource;
-import net.crystalixs.core.paper.command.PaperPlayerAbstractCommandSource;
+import net.crystalixs.core.paper.command.PaperCommandSource;
+import net.crystalixs.core.paper.command.PaperPlayerCommandSource;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,21 +25,21 @@ public class CorePlugin extends JavaPlugin {
     }
 
     private void registerCommands() {
-        PaperCommandManager<PaperAbstractCommandSource> commandManager = PaperCommandManager.builder(senderMapper())
-                .executionCoordinator(ExecutionCoordinator.<PaperAbstractCommandSource>builder().build())
+        PaperCommandManager<PaperCommandSource> commandManager = PaperCommandManager.builder(senderMapper())
+                .executionCoordinator(ExecutionCoordinator.<PaperCommandSource>builder().build())
                 .buildOnEnable(this);
 
         // Hier Commands registrieren
     }
 
-    private @NotNull SenderMapper<CommandSourceStack, PaperAbstractCommandSource> senderMapper() {
+    private @NotNull SenderMapper<CommandSourceStack, PaperCommandSource> senderMapper() {
         return SenderMapper.create(commandSourceStack -> {
             CommandSender sender = commandSourceStack.getSender();
             return sender instanceof Player player
-                    ? new PaperPlayerAbstractCommandSource(player, commandSourceStack)
-                    : new PaperAbstractCommandSource(sender, commandSourceStack);
+                    ? new PaperPlayerCommandSource(player, commandSourceStack)
+                    : new PaperCommandSource(sender, commandSourceStack);
 
-        }, PaperAbstractCommandSource::commandSourceStack);
+        }, PaperCommandSource::commandSourceStack);
     }
 
 }
--- paper/src/main/java/net/crystalixs/core/paper/command/PaperPlayerCommandSource.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/PaperPlayerCommandSource.java
@@ -3,7 +3,7 @@ package net.crystalixs.core.paper.command;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.bukkit.command.CommandSender;
 
-public class PaperPlayerCommandSource extends PaperAbstractCommandSource {
+public class PaperPlayerCommandSource extends PaperCommandSource {
 
     public PaperPlayerCommandSource(CommandSender plattformSender, CommandSourceStack commandSourceStack) {
         super(plattformSender, commandSourceStack);
