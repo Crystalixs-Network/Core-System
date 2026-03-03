a591b9f8ec4c6cc79d8113cd4361cff8d8648c86
Merry <svgr1808@outlook.de>
2026-03-03 15:15:48 +0100

    Add maintenance mode support to configuration and MOTD handling

---
+++ velocity/src/main/java/net/crystalixs/core/velocity/config/Maintenance.java
@@ -0,0 +1,8 @@
+package net.crystalixs.core.velocity.config;
+
+import net.kyori.adventure.text.Component;
+
+public record Maintenance(boolean enabled, String version, Motd motd, Screen screen) {
+    record Screen(Component header, Component body, Component footer, Component url) {
+    }
+}
--- velocity/src/main/java/net/crystalixs/core/velocity/listener/MotdListener.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/listener/MotdListener.java
@@ -3,8 +3,10 @@ package net.crystalixs.core.velocity.listener;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.proxy.server.ServerPing;
+import net.crystalixs.core.velocity.config.Maintenance;
 import net.crystalixs.core.velocity.config.Motd;
 import net.crystalixs.core.velocity.config.VelocityConfig;
+import net.kyori.adventure.text.Component;
 
 import static net.kyori.adventure.text.Component.newline;
 
@@ -18,12 +20,24 @@ public class MotdListener {
 
     @Subscribe
     public void onProxyPing(ProxyPingEvent event) {
-        Motd motd = config.motd();
-        ServerPing ping = event.getPing().asBuilder()
-                .description(motd.firstLine().append(newline()).append(motd.secondLine()))
-                .build();
+        event.setPing(createPing(event.getPing().asBuilder()));
+    }
 
-        event.setPing(ping);
+    private ServerPing createPing(ServerPing.Builder builder) {
+        Maintenance maintenance = config.maintenance();
+        if (maintenance.enabled()) {
+            builder.version(new ServerPing.Version(-1, maintenance.version()));
+        }
+        builder.description(descriptionComponent());
+
+        return builder.build();
+    }
+
+    private Component descriptionComponent() {
+        Motd motd = config.maintenance().enabled() ? config.maintenance().motd() : config.motd();
+        return motd.firstLine()
+                .append(newline())
+                .append(motd.secondLine());
     }
 
 }
