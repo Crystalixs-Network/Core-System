a3e0a373cddb52d6e36c2db78e5e5b3d57dd4dff
Merry <svgr1808@outlook.de>
2026-03-04 00:20:44 +0100

    Improve imports

--- velocity/src/test/kotlin/net/crystalixs/core/velocity/test/config/translation/VelocityTranslationRegistryMergeTest.kt
+++ velocity/src/test/kotlin/net/crystalixs/core/velocity/test/config/translation/VelocityTranslationRegistryMergeTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
 import net.crystalixs.core.velocity.config.translation.VelocityTranslationRegistry
-import java.util.Properties
+import java.util.*
 
 class VelocityTranslationRegistryMergeTest : FunSpec({
 
