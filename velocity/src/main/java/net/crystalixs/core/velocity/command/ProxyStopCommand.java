c53eb68c4ede2d05d2a06c0eb4943704c5ee9790
Merry <svgr1808@outlook.de>
2026-03-05 00:34:07 +0100

    Implement Proxy-Stop command

---
+++ velocity/src/main/java/net/crystalixs/core/velocity/command/ProxyStopCommand.java
@@ -0,0 +1,32 @@
+package net.crystalixs.core.velocity.command;
+
+import com.velocitypowered.api.proxy.ProxyServer;
+import net.crystalixs.core.velocity.CorePlugin;
+import net.crystalixs.core.velocity.command.cloud.VelocityCommand;
+import net.crystalixs.core.velocity.command.cloud.VelocityCommandSource;
+import net.crystalixs.core.velocity.command.cloud.VelocityPlayerCommandSource;
+import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.permission.Permission;
+import org.jspecify.annotations.NonNull;
+
+import static org.incendo.cloud.minecraft.extras.RichDescription.translatable;
+
+public class ProxyStopCommand extends VelocityCommand {
+
+    private final ProxyServer server;
+
+    public ProxyStopCommand(CorePlugin plugin, ProxyServer server) {
+        super(plugin);
+        this.server = server;
+    }
+
+    @Override
+    public void registerTo(@NonNull CommandManager<VelocityCommandSource> commandManager) {
+        commandManager.command(commandManager.commandBuilder("proxy-stop")
+                .commandDescription(translatable("command.proxy-stop.description"))
+                .senderType(VelocityPlayerCommandSource.class)
+                .permission(Permission.of("core.command.proxy-stop"))
+                .handler(context -> server.shutdown())
+        );
+    }
+}
