025871a16190abf96f1029e8b5ee1b317ffc1028
Merry <svgr1808@outlook.de>
2026-03-05 20:03:54 +0100

    Add status message

--- velocity/src/main/java/net/crystalixs/core/velocity/command/OnlineCommand.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/command/OnlineCommand.java
@@ -2,17 +2,19 @@ package net.crystalixs.core.velocity.command;
 
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import kotlin._Assertions;
 import net.crystalixs.core.velocity.CorePlugin;
 import net.crystalixs.core.velocity.command.cloud.VelocityCommand;
 import net.crystalixs.core.velocity.command.cloud.VelocityCommandSource;
 import net.crystalixs.core.velocity.command.cloud.VelocityPlayerCommandSource;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
+import net.kyori.adventure.text.minimessage.translation.Argument;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.minecraft.extras.RichDescription;
 import org.incendo.cloud.permission.Permission;
 import org.jspecify.annotations.NonNull;
 
 import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.translatable;
 import static org.incendo.cloud.velocity.parser.ServerParser.serverParser;
 
 public class OnlineCommand extends VelocityCommand {
@@ -32,14 +34,10 @@ public class OnlineCommand extends VelocityCommand {
                     Player source = context.sender().player();
                     RegisteredServer requested = context.get("server");
 
-                    requested.ping().whenComplete(((ping, throwable) -> {
-                        if (ping == null || throwable != null) {
-                            source.sendMessage(text("Server offline. exception"));
-                            return;
-                        }
-
-                        source.sendMessage(text("Server online"));
-                    }));
+                    requested.ping().whenComplete(((ping, throwable) -> source.sendMessage(translatable("command.online").arguments(
+                            Argument.component("server", text(requested.getServerInfo().getName())),
+                            Argument.tagResolver(Formatter.booleanChoice("status", throwable == null || ping != null))
+                    ))));
                 })
         );
     }
