7f1eb92a1c85d0912dfc5205b53329e2ec5c2d76
Merry <svgr1808@outlook.de>
2026-03-06 16:35:04 +0100

    [CS-12] Rework translation registration

--- common/src/main/java/net/crystalixs/core/common/translation/TranslationRegistry.java
+++ common/src/main/java/net/crystalixs/core/common/translation/TranslationRegistry.java
@@ -1,27 +1,34 @@
 package net.crystalixs.core.common.translation;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.key.KeyPattern.Value;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.translation.MiniMessageTranslationStore;
+import net.kyori.adventure.translation.GlobalTranslator;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Locale;
 
-import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.key.Key.key;
 
 public final class TranslationRegistry {
 
-    private final Map<String, Component> translations = new ConcurrentHashMap<>();
+    private final MiniMessage miniMessage;
+    private MiniMessageTranslationStore store;
 
-    public void register(String key, Component component) {
-        translations.put(key, component);
+    private final Locale defaultLocale;
+
+    public TranslationRegistry(MiniMessage miniMessage, Locale defaultLocale) {
+        this.miniMessage = miniMessage;
+        this.defaultLocale = defaultLocale;
     }
 
-    public Component get(String key) {
-        Component result = translations.get(key);
-        if (result == null) {
-            return text("Missing translation: " + key, NamedTextColor.RED);
-        }
-        return result;
+    public void registerBundle(@Value String bundleName, Locale... locales) {
+        store = MiniMessageTranslationStore.create(key("crystalixs", bundleName), miniMessage);
+        store.defaultLocale(defaultLocale);
+
+        GlobalTranslator.translator().addSource(store);
     }
 
+    public MiniMessageTranslationStore store() {
+        return store;
+    }
 }
