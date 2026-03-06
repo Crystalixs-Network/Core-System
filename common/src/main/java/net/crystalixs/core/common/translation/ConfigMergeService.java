5bae8ab7fa8a8ab5a54495ecdaf02b1f9120f143
Merry <svgr1808@outlook.de>
2026-03-06 16:16:39 +0100

    [CS-12] Implement config merging

---
+++ common/src/main/java/net/crystalixs/core/common/translation/ConfigMergeService.java
@@ -0,0 +1,34 @@
+package net.crystalixs.core.common.translation;
+
+import org.spongepowered.configurate.ConfigurationNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class ConfigMergeService {
+
+    public void merge(ConfigurationNode defaults, ConfigurationNode user) {
+        // Add missing defaults
+        for (var entry : defaults.childrenMap().entrySet()) {
+            Object key = entry.getKey();
+            ConfigurationNode defaultChild = entry.getValue();
+            ConfigurationNode userChild = user.node(key);
+
+            if (userChild.virtual()) {
+                userChild.from(defaultChild);
+            } else {
+                merge(defaultChild, userChild);
+            }
+        }
+
+        // Remove old keys that don't exist in defaults
+        Set<Object> toRemove = new HashSet<>();
+        for (Object key : user.childrenMap().keySet()) {
+            if (!defaults.childrenMap().containsKey(key)) {
+                toRemove.add(key);
+            }
+        }
+        toRemove.forEach(object -> user.node(object).raw(null));
+    }
+
+}
