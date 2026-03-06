a22867cbec2455166bbdfea0ed102c81878a99fc
Merry <svgr1808@outlook.de>
2026-03-06 21:40:33 +0100

    [CS-12] Refactor core reload command

--- velocity/src/main/java/net/crystalixs/core/velocity/command/CoreCommand.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/command/CoreCommand.java
@@ -1,23 +1,28 @@
 package net.crystalixs.core.velocity.command;
 
+import net.crystalixs.core.common.translation.TranslationProvider;
 import net.crystalixs.core.velocity.CorePlugin;
 import net.crystalixs.core.velocity.command.cloud.VelocityCommand;
 import net.crystalixs.core.velocity.command.cloud.VelocityCommandSource;
 import net.crystalixs.core.velocity.config.VelocityConfigLoader;
 import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.minecraft.extras.RichDescription;
 import org.incendo.cloud.permission.Permission;
 import org.jspecify.annotations.NonNull;
 
 import static net.kyori.adventure.text.Component.translatable;
+import static org.incendo.cloud.parser.standard.StringParser.stringParser;
 
 public class CoreCommand extends VelocityCommand {
 
     private final VelocityConfigLoader loader;
+    private final TranslationProvider provider;
 
-    public CoreCommand(CorePlugin plugin, VelocityConfigLoader loader) {
+    public CoreCommand(CorePlugin plugin, VelocityConfigLoader loader, TranslationProvider provider) {
         super(plugin);
         this.loader = loader;
+        this.provider = provider;
     }
 
     @Override
@@ -26,10 +31,31 @@ public class CoreCommand extends VelocityCommand {
                 .commandDescription(RichDescription.translatable("command.core.description.main"))
                 .senderType(VelocityCommandSource.class)
                 .permission(Permission.of("core.command.core"))
-                .literal("reload", RichDescription.translatable("command.core.description.reload"))
+                .literal("reload", RichDescription.translatable("command.core.description.reload.main"))
+                .optional("mode", stringParser(), RichDescription.translatable("command.core.description.reload.mode"))
                 .handler(context -> {
-                    loader.saveAndReload();
-                    context.sender().plattformSender().sendMessage(translatable("command.core.reload"));
+                    String mode = context.getOrDefault("mode", "all");
+
+                    switch (mode.toLowerCase()) {
+                        case "config" -> reloadConfig(context);
+                        case "messages" -> reloadMessages(context);
+                        default -> reloadAll(context);
+                    }
                 }));
+    }
+
+    private void reloadAll(CommandContext<VelocityCommandSource> context) {
+        reloadConfig(context);
+        reloadMessages(context);
+    }
+
+    private void reloadMessages(CommandContext<VelocityCommandSource> context) {
+        provider.reload();
+        context.sender().plattformSender().sendMessage(translatable());
+    }
+
+    private void reloadConfig(CommandContext<VelocityCommandSource> context) {
+        loader.saveAndReload();
+        context.sender().plattformSender().sendMessage(translatable("command.core.reload"));
     }
 }
