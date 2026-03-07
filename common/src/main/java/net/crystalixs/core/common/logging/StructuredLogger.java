6a8f57447f2239392b63bfb6eb0e4e411545047a
Merry <svgr1808@outlook.de>
2026-03-07 19:54:09 +0100

    Implement abstract logger

---
+++ common/src/main/java/net/crystalixs/core/common/logging/StructuredLogger.java
@@ -0,0 +1,20 @@
+package net.crystalixs.core.common.logging;
+
+public interface StructuredLogger {
+
+    StructuredLogger child(String component);
+
+    void info(String event);
+
+    void info(String event, LogMetadata fields);
+
+    void warn(String event);
+
+    void warn(String event, LogMetadata fields);
+
+    void warn(String event, LogMetadata fields, Throwable throwable);
+
+    void error(String event, LogMetadata fields, Throwable throwable);
+}
+
+
