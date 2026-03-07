525fc6c161f6a503227e7fd27bfc43f27844e6cb
Merry <svgr1808@outlook.de>
2026-03-07 19:55:31 +0100

    Rework change logger

---
+++ common/src/main/java/net/crystalixs/core/common/logging/ChangeSetLogger.java
@@ -0,0 +1,31 @@
+package net.crystalixs.core.common.logging;
+
+public interface ChangeSetLogger {
+
+    void log(StructuredLogger logger, String subject, String fileName, ChangeSet changeSet);
+
+    static ChangeSetLogger createDefault() {
+        return new DefaultChangeSetLogger();
+    }
+
+    final class DefaultChangeSetLogger implements ChangeSetLogger {
+
+        @Override
+        public void log(StructuredLogger logger, String subject, String fileName, ChangeSet changeSet) {
+            if (!changeSet.hasChanges()) {
+                return;
+            }
+
+            StructuredLogger syncLogger = logger.child("sync");
+            syncLogger.info("synchronized changes", LogMetadata.of("subject", subject)
+                    .and("file", fileName)
+                    .and("entries", changeSet.size()));
+
+            for (ChangeSet.Entry entry : changeSet.getEntries()) {
+                syncLogger.info(entry.render());
+            }
+        }
+    }
+}
+
+
