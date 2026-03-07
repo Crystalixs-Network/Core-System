8dc8b9950e063afc71d81110d541bea7491d8ce8
Merry <svgr1808@outlook.de>
2026-03-07 15:50:58 +0100

    [CV-16] Make facade composition more explicit

--- velocity/src/main/java/net/crystalixs/core/velocity/config/VelocityConfigUpdater.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/config/VelocityConfigUpdater.java
@@ -13,6 +13,18 @@ public final class VelocityConfigUpdater {
         this.facade = new ConfigFacade<>(service);
     }
 
+    public VelocityConfig current() {
+        return facade.current();
+    }
+
+    public void save() throws IOException {
+        facade.save();
+    }
+
+    public void reload() throws IOException {
+        facade.reload();
+    }
+
     public void setMaintenance(boolean enabled) throws IOException {
         facade.update(config -> config.withMaintenance(enabled
                 ? config.maintenance().enable()
