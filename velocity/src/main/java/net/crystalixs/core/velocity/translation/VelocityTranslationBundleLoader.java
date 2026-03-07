11b7e798c75f22f24598e7e58fb7c777f624e740
Merry <svgr1808@outlook.de>
2026-03-07 18:06:58 +0100

    Improve bundle loading

--- velocity/src/main/java/net/crystalixs/core/velocity/translation/VelocityTranslationBundleLoader.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/translation/VelocityTranslationBundleLoader.java
@@ -1,31 +1,19 @@
 package net.crystalixs.core.velocity.translation;
 
-import net.crystalixs.core.common.translation.TranslationConfigMergeService;
-import net.crystalixs.core.common.translation.TranslationBundle;
-import net.crystalixs.core.common.translation.TranslationBundleLoader;
-import net.crystalixs.core.common.translation.TranslationFlattener;
-import org.spongepowered.configurate.CommentedConfigurationNode;
-import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
+import net.crystalixs.core.common.translation.AbstractTranslationBundleLoader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.io.InputStream;
 import java.nio.file.Path;
-import java.util.Locale;
-import java.util.Map;
+import java.util.logging.Logger;
 
-public final class VelocityTranslationBundleLoader implements TranslationBundleLoader {
+public final class VelocityTranslationBundleLoader extends AbstractTranslationBundleLoader {
 
-    private final TranslationConfigMergeService service;
-    private final TranslationFlattener flattener;
     private final Path dataDirectory;
+    private final Logger logger;
 
-    private VelocityTranslationBundleLoader(Path dataDirectory, TranslationConfigMergeService service, TranslationFlattener flattener) {
+    private VelocityTranslationBundleLoader(Path dataDirectory, Logger logger) {
         this.dataDirectory = dataDirectory;
-        this.service = service;
-        this.flattener = flattener;
+        this.logger = logger;
     }
 
     public static Builder builder() {
@@ -33,56 +21,37 @@ public final class VelocityTranslationBundleLoader implements TranslationBundleL
     }
 
     @Override
-    public TranslationBundle load(String bundleName, Locale locale) throws IOException {
-        String tag = locale.toLanguageTag().replace('-', '_').toLowerCase();
-        String fileName = bundleName + "_" + tag + ".conf";
-
-        Path langDirectory = dataDirectory.resolve("lang");
-        Path userFile = langDirectory.resolve(fileName);
-        Files.createDirectories(userFile.getParent());
-
-        return loadFromResources(locale, fileName, userFile);
+    protected Path resolveDataDirectory() {
+        return dataDirectory;
     }
 
-    private TranslationBundle loadFromResources(Locale locale, String fileName, Path userFile) throws IOException {
-        try (var stream = getClass().getClassLoader().getResourceAsStream("lang/" + fileName)) {
-            if (stream == null) {
-                throw new IllegalStateException("Missing translation resource: " + fileName);
-            }
+    @Override
+    protected InputStream openResource(String fileName) {
+        return getClass().getClassLoader().getResourceAsStream("lang/" + fileName);
+    }
 
-            HoconConfigurationLoader defaultLoader = HoconConfigurationLoader.builder()
-                    .source(() -> new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)))
-                    .build();
-            CommentedConfigurationNode defaultNode = defaultLoader.load();
-
-            HoconConfigurationLoader fileLoader = HoconConfigurationLoader.builder()
-                    .prettyPrinting(true)
-                    .emitComments(true)
-                    .path(userFile)
-                    .build();
-            CommentedConfigurationNode userNode = Files.notExists(userFile) ? defaultNode.copy() : fileLoader.load();
-
-            service.merge(defaultNode, userNode);
-            fileLoader.save(userNode);
-
-            Map<String, String> flattened = flattener.flattern(userNode);
-            return new TranslationBundle(locale, flattened);
-
-        } catch (Exception exception) {
-            throw new IOException("Failed to load bundle " + fileName, exception);
-        }
+    @Override
+    protected Logger logger() {
+        return logger;
     }
 
     public static final class Builder {
         private Path dataDirectory;
+        private Logger logger;
 
         public Builder dataDirectory(Path dataDirectory) {
             this.dataDirectory = dataDirectory;
             return this;
         }
 
+        public Builder logger(Logger logger) {
+            this.logger = logger;
+            return this;
+        }
+
         public VelocityTranslationBundleLoader build() {
-            return new VelocityTranslationBundleLoader(dataDirectory, new TranslationConfigMergeService(), new TranslationFlattener());
+            Logger effectiveLogger = logger == null ? Logger.getLogger(VelocityTranslationBundleLoader.class.getSimpleName()) : logger;
+            return new VelocityTranslationBundleLoader(dataDirectory, effectiveLogger);
         }
     }
 }
