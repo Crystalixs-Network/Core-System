7e8ea20fa6d78bc21042b11c5719072e056d5025
Merry <svgr1808@outlook.de>
2026-03-07 03:22:45 +0100

    [CV-16] Simplify logger

--- common/src/main/java/net/crystalixs/core/common/config/ConfigChangeLogger.java
+++ common/src/main/java/net/crystalixs/core/common/config/ConfigChangeLogger.java
@@ -2,8 +2,14 @@ package net.crystalixs.core.common.config;
 
 import java.util.logging.Logger;
 
-public interface ConfigChangeLogger {
+public final class ConfigChangeLogger {
 
-    void log(Logger logger, String fileName, ConfigChangeSet changeSet);
+    public void log(Logger logger, String fileName, ConfigChangeSet changeSet) {
+        if (!changeSet.hasChanges()) return;
 
+        logger.info("Synchronized config " + fileName + ":");
+        for (String entry : changeSet.getEntries()) {
+            logger.info("  " + entry);
+        }
+    }
 }
