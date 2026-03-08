8bedd976d2fc02d22f7dd4f3e4d6e596139d8325
Merry <svgr1808@outlook.de>
2026-03-08 21:27:09 +0100

    Import static Component#translatable

--- velocity/src/main/java/net/crystalixs/core/velocity/CorePlugin.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/CorePlugin.java
@@ -14,8 +14,8 @@ import net.crystalixs.core.common.config.ConfigService;
 import net.crystalixs.core.common.config.ConfigServiceFactory;
 import net.crystalixs.core.common.logging.LogFactory;
 import net.crystalixs.core.common.logging.LogManager;
-import net.crystalixs.core.common.logging.StructuredLogger;
 import net.crystalixs.core.common.logging.LogMetadata;
+import net.crystalixs.core.common.logging.StructuredLogger;
 import net.crystalixs.core.common.translation.HotReloadWatcher;
 import net.crystalixs.core.common.translation.TranslationBundleMeta;
 import net.crystalixs.core.common.translation.TranslationProvider;
@@ -28,7 +28,6 @@ import net.crystalixs.core.velocity.config.platform.VelocityConfigurationProvide
 import net.crystalixs.core.velocity.listener.MotdListener;
 import net.crystalixs.core.velocity.listener.PlayerConnectionListener;
 import net.crystalixs.core.velocity.translation.VelocityTranslationBundleLoader;
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import org.incendo.cloud.SenderMapper;
@@ -51,7 +50,7 @@ public final class CorePlugin {
 
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private final MiniMessage miniMessage = MiniMessage.builder()
-            .editTags(builder -> builder.tag("prefix", Tag.inserting(Component.translatable("prefix"))))
+            .editTags(builder -> builder.tag("prefix", Tag.inserting(translatable("prefix"))))
             .build();
 
     private final PluginContainer pluginContainer;
