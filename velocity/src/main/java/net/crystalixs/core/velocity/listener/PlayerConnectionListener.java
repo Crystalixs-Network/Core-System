5ef580433176e003a28e62d3b9bd43d25cbcf730
Merry <svgr1808@outlook.de>
2026-03-08 21:24:30 +0100

    Send header and footer to the player

--- velocity/src/main/java/net/crystalixs/core/velocity/listener/PlayerConnectionListener.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/listener/PlayerConnectionListener.java
@@ -2,7 +2,9 @@ package net.crystalixs.core.velocity.listener;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.proxy.Player;
+import net.crystalixs.core.velocity.config.Tablist;
 import net.crystalixs.core.velocity.config.VelocityConfig;
 import net.crystalixs.core.velocity.config.platform.VelocityConfigUpdater;
 import net.kyori.adventure.text.minimessage.MiniMessage;
@@ -26,6 +28,16 @@ public class PlayerConnectionListener {
         if (player.hasPermission("core.bypass.maintenance")) return;
 
         player.disconnect(config.maintenance().screen().construct(miniMessage));
+    }
+
+    @Subscribe
+    public void onBackendConnect(ServerPostConnectEvent event) {
+        Player player = event.getPlayer();
+        Tablist tablist = updater.current().tablist();
+
+        player.sendPlayerListHeaderAndFooter(
+                tablist.headerComponent(miniMessage),
+                tablist.footerComponent(miniMessage));
     }
 
 }
