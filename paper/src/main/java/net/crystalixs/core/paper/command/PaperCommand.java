0d4b3434d1dbccf3eb9316e6fb641ae2e6ea40f0
Merry <svgr1808@outlook.de>
2026-03-08 23:03:26 +0100

    Make paper command logging type-safe

--- paper/src/main/java/net/crystalixs/core/paper/command/PaperCommand.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/PaperCommand.java
@@ -3,18 +3,14 @@ package net.crystalixs.core.paper.command;
 import net.crystalixs.core.common.command.AbstractCommand;
 import net.crystalixs.core.common.logging.StructuredLogger;
 import net.crystalixs.core.paper.CorePlugin;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public abstract class PaperCommand<I extends JavaPlugin> extends AbstractCommand<PaperCommandSource, I> {
+public abstract class PaperCommand extends AbstractCommand<PaperCommandSource, CorePlugin> {
 
-    public PaperCommand(I plugin) {
+    public PaperCommand(CorePlugin plugin) {
         super(plugin);
     }
 
     protected StructuredLogger commandLogger(String commandName) {
-        if (plugin instanceof CorePlugin corePlugin) {
-            return corePlugin.commandLogger(commandName);
-        }
-        throw new IllegalStateException("Paper command logger is only available for CorePlugin");
+        return plugin.commandLogger(commandName);
     }
 }
