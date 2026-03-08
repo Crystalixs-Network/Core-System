dc3d9bbcf2b44222491609e347d4783b0a106f5c
Merry <svgr1808@outlook.de>
2026-03-08 22:31:49 +0100

    Implement velocity platform bootstrap

--- velocity/src/main/java/net/crystalixs/core/velocity/bootstrap/VelocityPluginBootstrap.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/bootstrap/VelocityPluginBootstrap.java
@@ -2,17 +2,16 @@ package net.crystalixs.core.velocity.bootstrap;
 
 import com.velocitypowered.api.plugin.PluginContainer;
 import com.velocitypowered.api.proxy.ProxyServer;
-import net.crystalixs.core.common.logging.LogMetadata;
+import net.crystalixs.core.common.bootstrap.AbstractPluginBootstrap;
 import net.crystalixs.core.velocity.CorePlugin;
 import net.crystalixs.core.velocity.config.platform.VelocityConfigUpdater;
 import org.slf4j.Logger;
 
 import java.nio.file.Path;
 
-public final class VelocityPluginBootstrap {
+public final class VelocityPluginBootstrap extends AbstractPluginBootstrap<VelocityPluginRuntime> {
 
     private final CorePlugin plugin;
-    private final VelocityPluginRuntime runtime;
     private final VelocityConfigBootstrap configBootstrap;
     private final VelocityCommandBootstrap commandBootstrap;
     private final VelocityListenerBootstrap listenerBootstrap;
@@ -21,46 +20,38 @@ public final class VelocityPluginBootstrap {
     private VelocityTranslationBootstrap translationBootstrap;
 
     private VelocityPluginBootstrap(CorePlugin plugin, VelocityPluginRuntime runtime, VelocityConfigBootstrap configBootstrap, VelocityCommandBootstrap commandBootstrap, VelocityListenerBootstrap listenerBootstrap) {
+        super(runtime);
         this.plugin = plugin;
-        this.runtime = runtime;
         this.configBootstrap = configBootstrap;
         this.commandBootstrap = commandBootstrap;
         this.listenerBootstrap = listenerBootstrap;
     }
 
     public static VelocityPluginBootstrap create(CorePlugin plugin, PluginContainer pluginContainer, ProxyServer server, Path dataDirectory, Logger platformLogger) {
-        return new VelocityPluginBootstrap(plugin,
-                VelocityPluginRuntime.create(pluginContainer, server, dataDirectory, platformLogger),
+        return new VelocityPluginBootstrap(plugin, VelocityPluginRuntime.create(pluginContainer, server, dataDirectory, platformLogger),
                 new VelocityConfigBootstrap(),
                 new VelocityCommandBootstrap(),
                 new VelocityListenerBootstrap());
     }
 
-    public void enable() {
+    @Override
+    protected void enableInternal() {
+        VelocityPluginRuntime runtime = runtime();
         configUpdater = configBootstrap.load(runtime);
         translationBootstrap = VelocityTranslationBootstrap.create(runtime, configUpdater);
         commandBootstrap.register(plugin, runtime, configUpdater, translationBootstrap.provider());
         listenerBootstrap.register(plugin, runtime, configUpdater);
-
-        runtime.logger().info("plugin enabled", LogMetadata.event("plugin.enabled"));
     }
 
-    public void disable() {
+    @Override
+    protected void disableInternal() {
+        VelocityPluginRuntime runtime = runtime();
         try {
             if (translationBootstrap != null) {
                 translationBootstrap.close();
             }
         } finally {
             configBootstrap.save(runtime, configUpdater);
-            try {
-                runtime.logger().info("plugin disabled", LogMetadata.event("plugin.disabled"));
-            } finally {
-                runtime.close();
-            }
         }
-    }
-
-    public VelocityPluginRuntime runtime() {
-        return runtime;
     }
 }
--- velocity/src/main/java/net/crystalixs/core/velocity/bootstrap/VelocityPluginRuntime.java
+++ velocity/src/main/java/net/crystalixs/core/velocity/bootstrap/VelocityPluginRuntime.java
@@ -4,18 +4,16 @@ import com.velocitypowered.api.plugin.PluginContainer;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.crystalixs.core.common.logging.LogFactory;
 import net.crystalixs.core.common.logging.LogManager;
-import net.crystalixs.core.common.logging.StructuredLogger;
+import net.crystalixs.core.common.bootstrap.AbstractPluginRuntime;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import org.slf4j.Logger;
 
 import java.nio.file.Path;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 import static net.kyori.adventure.text.Component.translatable;
 
-public final class VelocityPluginRuntime implements AutoCloseable {
+public final class VelocityPluginRuntime extends AbstractPluginRuntime {
 
     private static final MiniMessage MINI_MESSAGE = MiniMessage.builder()
             .editTags(builder -> builder.tag("prefix", Tag.inserting(translatable("prefix"))))
@@ -24,27 +22,17 @@ public final class VelocityPluginRuntime implements AutoCloseable {
     private final PluginContainer pluginContainer;
     private final ProxyServer server;
     private final Path dataDirectory;
-    private final ScheduledExecutorService scheduler;
-    private final MiniMessage miniMessage;
-    private final LogFactory logging;
-    private final StructuredLogger logger;
 
-    private VelocityPluginRuntime(PluginContainer pluginContainer, ProxyServer server, Path dataDirectory, ScheduledExecutorService scheduler, MiniMessage miniMessage, LogFactory logging, StructuredLogger logger) {
+    private VelocityPluginRuntime(PluginContainer pluginContainer, ProxyServer server, Path dataDirectory, LogFactory logging) {
+        super(logging);
         this.pluginContainer = pluginContainer;
         this.server = server;
         this.dataDirectory = dataDirectory;
-        this.scheduler = scheduler;
-        this.miniMessage = miniMessage;
-        this.logging = logging;
-        this.logger = logger;
     }
 
     public static VelocityPluginRuntime create(PluginContainer pluginContainer, ProxyServer server, Path dataDirectory, Logger platformLogger) {
         LogFactory logging = LogManager.createForSlf4j(platformLogger, dataDirectory.resolve("logs"));
-        return new VelocityPluginRuntime(pluginContainer, server, dataDirectory,
-                Executors.newSingleThreadScheduledExecutor(),
-                MINI_MESSAGE,
-                logging, logging.logger("core"));
+        return new VelocityPluginRuntime(pluginContainer, server, dataDirectory, logging);
     }
 
     public PluginContainer pluginContainer() {
@@ -59,21 +47,7 @@ public final class VelocityPluginRuntime implements AutoCloseable {
         return dataDirectory;
     }
 
-    public ScheduledExecutorService scheduler() {
-        return scheduler;
-    }
-
     public MiniMessage miniMessage() {
-        return miniMessage;
-    }
-
-    public StructuredLogger logger() {
-        return logger;
-    }
-
-    @Override
-    public void close() {
-        scheduler.shutdownNow();
-        logging.close();
+        return MINI_MESSAGE;
     }
 }
