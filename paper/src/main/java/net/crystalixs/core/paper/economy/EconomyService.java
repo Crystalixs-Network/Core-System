87f59510fbea5019563c4912a97abbadd76e01ea
Merry <svgr1808@outlook.de>
2026-03-10 00:51:44 +0100

    Add economy service methods for set/take/add and transferCoin operations

--- paper/src/main/java/net/crystalixs/core/paper/economy/EconomyService.java
+++ paper/src/main/java/net/crystalixs/core/paper/economy/EconomyService.java
@@ -1,5 +1,7 @@
 package net.crystalixs.core.paper.economy;
 
+import net.crystalixs.core.persistence.model.Currency;
+
 import java.util.UUID;
 
 public interface EconomyService {
@@ -9,5 +11,13 @@ public interface EconomyService {
     long getCoins(UUID playerId);
 
     long getGems(UUID playerId);
+
+    void addCurrency(UUID playerId, Currency currency, long amount);
+
+    void setCurrency(UUID playerId, Currency currency, long amount);
+
+    void takeCurrency(UUID playerId, Currency currency, long amount);
+
+    void transferCoins(UUID fromPlayerId, UUID toPlayerId, long amount);
 
 }
