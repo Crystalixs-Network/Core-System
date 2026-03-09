08d98e2f6a1816da09f491914c66d01ae89ccfde
Merry <svgr1808@outlook.de>
2026-03-09 23:43:45 +0100

    Add stores to PersistenceContext

--- persistence/src/main/java/net/crystalixs/core/persistence/api/PersistenceContext.java
+++ persistence/src/main/java/net/crystalixs/core/persistence/api/PersistenceContext.java
@@ -1,13 +1,19 @@
 package net.crystalixs.core.persistence.api;
 
+import net.crystalixs.core.persistence.store.AuditStore;
 import net.crystalixs.core.persistence.store.HomeStore;
 import net.crystalixs.core.persistence.store.PlayerStore;
+import net.crystalixs.core.persistence.store.TransactionStore;
 
 public interface PersistenceContext extends AutoCloseable {
 
     PlayerStore players();
 
     HomeStore homes();
+
+    TransactionStore transactions();
+
+    AuditStore audits();
 
     @Override
     void close();
