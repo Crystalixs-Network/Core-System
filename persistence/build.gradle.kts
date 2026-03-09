c00d3f29cd00af605ad4cfc6d54591fdadd39607
Merry <svgr1808@outlook.de>
2026-03-09 13:32:33 +0100

    Initialize persistence module with sadu dependencies

---
+++ persistence/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `java-library`
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    api(project(":common"))
+    implementation(libs.bundles.sadu)
+}
+
+tasks {
+    val artifact = project.mavenArtifact()
+
+    shadowJar {
+        val mapping = mapOf(
+            libs.sadu.datasource to "sadu.datasource",
+            libs.sadu.mariadb to "sadu.mariadb",
+            libs.sadu.queries to "sadu.queries",
+            libs.sadu.updater to "sadu.updater"
+        )
+
+        val base = "$group.$artifact.persistence.libs"
+        for ((dependency, name) in mapping) relocate(dependency.get().group, "$base.$name")
+    }
+
+    jar {
+        archiveBaseName.set("$artifact-persistence")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
