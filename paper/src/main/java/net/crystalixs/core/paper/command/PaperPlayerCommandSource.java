ff7e4170314f26aae6159e78ee9e158467ac661e
Merry <svgr1808@outlook.de>
2026-03-10 02:39:51 +0100

    Add helper method for player cast

--- paper/src/main/java/net/crystalixs/core/paper/command/PaperPlayerCommandSource.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/PaperPlayerCommandSource.java
@@ -2,10 +2,15 @@ package net.crystalixs.core.paper.command;
 
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PaperPlayerCommandSource extends PaperCommandSource {
 
     public PaperPlayerCommandSource(CommandSender plattformSender, CommandSourceStack commandSourceStack) {
         super(plattformSender, commandSourceStack);
+    }
+
+    public Player player() {
+        return (Player) plattformSender;
     }
 }
