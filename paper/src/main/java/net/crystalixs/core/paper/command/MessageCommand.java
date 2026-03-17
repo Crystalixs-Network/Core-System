34b0b948ad379df28355ed475f657db5250c0d29
Merry <svgr1808@outlook.de>
2026-03-17 14:40:01 +0100

    Add private message command

--- paper/src/main/java/net/crystalixs/core/paper/command/MessageCommand.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/MessageCommand.java
@@ -4,15 +4,26 @@ import net.crystalixs.core.paper.CorePlugin;
 import net.crystalixs.core.paper.command.cloud.PaperCommand;
 import net.crystalixs.core.paper.command.cloud.PaperCommandSource;
 import net.crystalixs.core.paper.command.cloud.PaperPlayerCommandSource;
+import org.bukkit.entity.Player;
 import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.context.CommandContext;
 import org.incendo.cloud.minecraft.extras.RichDescription;
 import org.incendo.cloud.permission.Permission;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static net.kyori.adventure.text.Component.translatable;
+import static net.kyori.adventure.text.minimessage.translation.Argument.component;
+import static net.kyori.adventure.text.minimessage.translation.Argument.string;
 import static org.incendo.cloud.bukkit.parser.PlayerParser.playerParser;
 import static org.incendo.cloud.parser.standard.StringParser.greedyStringParser;
 
 public class MessageCommand extends PaperCommand {
+
+    private final Map<UUID, UUID> lastMessageBySender = new HashMap<>();
 
     public MessageCommand(CorePlugin plugin) {
         super(plugin);
@@ -26,7 +37,27 @@ public class MessageCommand extends PaperCommand {
                 .permission(Permission.of("core.command.message"))
                 .required("player", playerParser(), RichDescription.translatable("command.message.description.player"))
                 .required("message", greedyStringParser(), RichDescription.translatable("command.message.description.message"))
-                .handler(context -> {
-                }));
+                .handler(this::handleMessage));
+    }
+
+    private void handleMessage(CommandContext<PaperPlayerCommandSource> context) {
+        Player sender = context.sender().player();
+        Player receiver = context.get("player");
+        String message = context.get("message");
+
+        if (sender.getUniqueId().equals(receiver.getUniqueId())) {
+            sender.sendMessage(translatable("command.message.error.self"));
+            return;
+        }
+
+        sender.sendMessage(translatable("command.message.sent").arguments(
+                component("player", receiver.name()),
+                string("message", message)));
+        receiver.sendMessage(translatable("command.message.received").arguments(
+                component("player", sender.name()),
+                string("message", message)));
+
+        lastMessageBySender.put(sender.getUniqueId(), receiver.getUniqueId());
+        lastMessageBySender.put(receiver.getUniqueId(), sender.getUniqueId());
     }
 }
