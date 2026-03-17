3776bc00f0f107415ce710bb0a443278eb496078
Merry <svgr1808@outlook.de>
2026-03-17 17:50:20 +0100

    Log inventory updates via InvUI post-update events

--- paper/src/main/java/net/crystalixs/core/paper/command/util/EnderchestViewService.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/util/EnderchestViewService.java
@@ -1,5 +1,6 @@
 package net.crystalixs.core.paper.command.util;
 
+import net.crystalixs.core.common.logging.LogMetadata;
 import net.crystalixs.core.common.logging.StructuredLogger;
 import net.crystalixs.core.paper.CorePlugin;
 import net.kyori.adventure.text.Component;
@@ -8,6 +9,8 @@ import org.bukkit.entity.Player;
 import xyz.xenondevs.inventoryaccess.component.AdventureComponentWrapper;
 import xyz.xenondevs.invui.gui.Gui;
 import xyz.xenondevs.invui.inventory.ReferencingInventory;
+import xyz.xenondevs.invui.inventory.event.ItemPostUpdateEvent;
+import xyz.xenondevs.invui.inventory.event.PlayerUpdateReason;
 import xyz.xenondevs.invui.window.Window;
 
 import static net.kyori.adventure.text.Component.translatable;
@@ -28,6 +31,8 @@ public final class EnderchestViewService {
 
         if (!canInteract) {
             reference.setPreUpdateHandler(event -> event.setCancelled(true));
+        } else {
+            reference.setPostUpdateHandler(event -> logUpdate(event, target));
         }
 
         String translationKey = viewer.equals(target) ? "command.enderchest.view.self" : "command.enderchest.view.other";
@@ -47,5 +52,17 @@ public final class EnderchestViewService {
                 .setGui(gui)
                 .setTitle(new AdventureComponentWrapper(renderedTitle))
                 .open(viewer);
+    }
+
+    private void logUpdate(ItemPostUpdateEvent event, Player chestOwner) {
+        if (!(event.getUpdateReason() instanceof PlayerUpdateReason reason)) return;
+
+        String action = event.isAdd() ? "add" : event.isRemove() ? "remove" : event.isSwap() ? "swap" : "update";
+        logger.info("enderchest inventory updated", LogMetadata
+                .event("command.enderchest.inventory.update")
+                .and(LogMetadata.Key.ACTOR, reason.getPlayer().getName())
+                .and(LogMetadata.Key.SUBJECT, chestOwner.getUniqueId().toString())
+                .and(LogMetadata.Key.DESCRIPTION, "slot=" + event.getSlot() + ", action=" + action)
+                .and(LogMetadata.Key.COMMAND, "enderchest"));
     }
 }
