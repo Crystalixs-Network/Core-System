afe7c284abfbd36149051dcf97b73463f83884f2
Merry <svgr1808@outlook.de>
2026-03-18 13:35:31 +0100

    Remove unused import

--- paper/src/main/java/net/crystalixs/core/paper/command/VanishCommand.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/VanishCommand.java
@@ -5,7 +5,6 @@ import net.crystalixs.core.paper.command.cloud.PaperCommand;
 import net.crystalixs.core.paper.command.cloud.PaperCommandSource;
 import net.crystalixs.core.paper.command.cloud.PaperPlayerCommandSource;
 import net.crystalixs.core.paper.command.util.VanishService;
-import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.minecraft.extras.RichDescription;
