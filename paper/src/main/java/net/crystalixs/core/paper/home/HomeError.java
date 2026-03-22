b2d91ee87ef5dea60a596796f218f0464e4b5cb2
Merry <svgr1808@outlook.de>
2026-03-22 17:35:16 +0100

    feat(home): add HOME_LIMIT_REACHED error

--- paper/src/main/java/net/crystalixs/core/paper/home/HomeError.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/HomeError.java
@@ -3,6 +3,7 @@ package net.crystalixs.core.paper.home;
 public enum HomeError {
     INVALID_NAME,
     INVALID_POSITION,
+    HOME_LIMIT_REACHED,
     HOME_ALREADY_EXISTS,
     PLAYER_CREATION_FAILED,
     HOME_CREATION_FAILED
