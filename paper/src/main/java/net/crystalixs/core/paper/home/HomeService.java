d98909b6e5d0aa907c252fe8ad5ae1c27762e2ce
Merry <svgr1808@outlook.de>
2026-03-22 23:11:38 +0100

    feat(home): add list contract to HomeService for gui data loading

--- paper/src/main/java/net/crystalixs/core/paper/home/HomeService.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/HomeService.java
@@ -3,6 +3,7 @@ package net.crystalixs.core.paper.home;
 import net.crystalixs.core.persistence.model.HomeModel;
 import org.bukkit.Location;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface HomeService {
@@ -10,6 +11,8 @@ public interface HomeService {
     HomeModel create(UUID playerId, String name, Location location);
 
     int count(UUID playerId);
+
+    Collection<HomeModel> all(UUID playerId);
 
     void delete(UUID playerId, String name);
 
