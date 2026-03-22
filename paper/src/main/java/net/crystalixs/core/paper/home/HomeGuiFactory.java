408083a102d4ec203eb34450af9d54a9ffccd464
Merry <svgr1808@outlook.de>
2026-03-22 23:04:44 +0100

    refactor(home-gui): define explicit slot keys for 27 home slots

--- paper/src/main/java/net/crystalixs/core/paper/home/HomeGuiFactory.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/HomeGuiFactory.java
@@ -9,9 +9,9 @@ public final class HomeGuiFactory {
     public void open(Player player) {
         Gui gui = Gui.normal()
                 .setStructure(
-                        "# # # # # # # # #",
-                        "# # # # # # # # #",
-                        "# # # # # # # # #"
+                        "a b c d e f g h i",
+                        "j k l m n o p q r",
+                        "s t u v w x y z 1"
                 )
                 .build();
 
