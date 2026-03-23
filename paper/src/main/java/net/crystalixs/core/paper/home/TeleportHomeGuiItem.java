efe4c22fa7bcc8196a13d574904f4149859234ea
Merry <svgr1808@outlook.de>
2026-03-23 23:13:43 +0100

    feat(home-gui): close homes gui after successful teleport click

--- paper/src/main/java/net/crystalixs/core/paper/home/TeleportHomeGuiItem.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/TeleportHomeGuiItem.java
@@ -30,6 +30,8 @@ public final class TeleportHomeGuiItem extends AbstractItem {
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
         if (!clickType.isLeftClick()) return;
+
         service.teleport(player, home.name());
+        player.closeInventory();
     }
 }
