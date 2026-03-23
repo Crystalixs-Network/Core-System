68b6e932e3d29ef9804ebd6047536b4b8d454434
Merry <svgr1808@outlook.de>
2026-03-23 23:10:15 +0100

    feat(home-gui): add clickable home item that triggers teleport

---
+++ paper/src/main/java/net/crystalixs/core/paper/home/TeleportHomeGuiItem.java
@@ -0,0 +1,34 @@
+package net.crystalixs.core.paper.home;
+
+import net.crystalixs.core.persistence.model.HomeModel;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.jetbrains.annotations.NotNull;
+import xyz.xenondevs.invui.item.ItemProvider;
+import xyz.xenondevs.invui.item.impl.AbstractItem;
+
+public final class TeleportHomeGuiItem extends AbstractItem {
+
+    private final Player player;
+    private final HomeModel home;
+    private final HomeService service;
+    private final HomeGuiItemFactory factory;
+
+    public TeleportHomeGuiItem(Player player, HomeModel home, HomeService service, HomeGuiItemFactory factory) {
+        this.player = player;
+        this.home = home;
+        this.service = service;
+        this.factory = factory;
+    }
+
+    @Override
+    public ItemProvider getItemProvider() {
+        return factory.icon(home);
+    }
+
+    @Override
+    public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
+        service.teleport(player, home.name());
+    }
+}
