a4fa5bd75a1a8d313846aa7d445bcc2657607eb0
Merry <svgr1808@outlook.de>
2026-03-25 22:10:52 +0100

    feat(home-gui): add reusable gui action item for edit menu

---
+++ paper/src/main/java/net/crystalixs/core/paper/home/HomeActionGuiItem.java
@@ -0,0 +1,31 @@
+package net.crystalixs.core.paper.home;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.jetbrains.annotations.NotNull;
+import xyz.xenondevs.invui.item.ItemProvider;
+import xyz.xenondevs.invui.item.impl.AbstractItem;
+
+import java.util.function.Consumer;
+
+public final class HomeActionGuiItem extends AbstractItem {
+
+    private final ItemProvider provider;
+    private final Consumer<Player> action;
+
+    public HomeActionGuiItem(ItemProvider provider, Consumer<Player> action) {
+        this.provider = provider;
+        this.action = action;
+    }
+
+    public ItemProvider provider() {
+        return provider;
+    }
+
+    @Override
+    public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
+        if (!clickType.isLeftClick()) return;
+        action.accept(player);
+    }
+}
