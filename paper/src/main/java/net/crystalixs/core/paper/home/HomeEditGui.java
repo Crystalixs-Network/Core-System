e1d89b47b2d63ef367fd53f153d3dc68ff486893
Merry <svgr1808@outlook.de>
2026-03-25 22:23:02 +0100

    refactor(home-gui): add dedicated edit gui screen type

---
+++ paper/src/main/java/net/crystalixs/core/paper/home/HomeEditGui.java
@@ -0,0 +1,35 @@
+package net.crystalixs.core.paper.home;
+
+import net.crystalixs.core.persistence.model.HomeModel;
+import org.bukkit.entity.Player;
+import xyz.xenondevs.invui.gui.Gui;
+import xyz.xenondevs.invui.item.impl.SimpleItem;
+
+final class HomeEditGui implements HomeGui {
+
+    private final HomeModel model;
+    private final HomeGuiItemFactory factory;
+
+    HomeEditGui(HomeModel model, HomeGuiItemFactory factory) {
+        this.model = model;
+        this.factory = factory;
+    }
+
+    @Override
+    public String titleKey() {
+        return "command.home.ui.edit.title";
+    }
+
+    @Override
+    public Gui buildGui(Player player) {
+        return Gui.normal()
+                .setStructure(
+                        "x x x x x x x x x",
+                        "x x x x i x x x x",
+                        "x x x x x x x x x"
+                )
+                .addIngredient('x', new SimpleItem(factory.locked()))
+                .addIngredient('i', new SimpleItem(factory.icon(model)))
+                .build();
+    }
+}
--- paper/src/main/java/net/crystalixs/core/paper/home/HomeGui.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/HomeGui.java
@@ -8,8 +8,7 @@ import xyz.xenondevs.invui.window.Window;
 
 import static net.kyori.adventure.text.Component.translatable;
 
-public sealed interface HomeGui
-        permits HomeListGui {
+public sealed interface HomeGui permits HomeListGui, HomeEditGui {
 
     default void open(Player player) {
         Gui gui = buildGui(player);
