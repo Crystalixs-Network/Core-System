d63ac53da54cc456705e12f1fa1eb0b910fd6ebe
Merry <svgr1808@outlook.de>
2026-03-25 22:49:19 +0100

    feat(home-gui): open edit screen on right click from home list

--- paper/src/main/java/net/crystalixs/core/paper/home/gui/item/TeleportHomeGuiItem.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/gui/item/TeleportHomeGuiItem.java
@@ -4,6 +4,7 @@ import net.crystalixs.core.paper.home.HomeException;
 import net.crystalixs.core.paper.home.HomeGuiFactory;
 import net.crystalixs.core.paper.home.HomeGuiItemFactory;
 import net.crystalixs.core.paper.home.HomeService;
+import net.crystalixs.core.paper.home.gui.HomeEditGui;
 import net.crystalixs.core.persistence.model.HomeModel;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
@@ -37,8 +38,12 @@ public final class TeleportHomeGuiItem extends AbstractItem {
 
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull InventoryClickEvent event) {
-        if (!clickType.isLeftClick()) return;
+        if (clickType.isRightClick()) {
+            guiFactory.open(player, HomeEditGui.class, home);
+            return;
+        }
 
+        if (!clickType.isLeftClick()) return;
         try {
             service.teleport(player, home.name());
             player.closeInventory();
