e61b8a7539d3c8d6932dea092121c38dc40d6884
Merry <svgr1808@outlook.de>
2026-03-26 18:09:51 +0100

    feat(home-gui): add item catalog

---
+++ paper/src/main/java/net/crystalixs/core/paper/home/gui/IconCatalog.java
@@ -0,0 +1,41 @@
+package net.crystalixs.core.paper.home.gui;
+
+import org.bukkit.Material;
+
+import java.util.List;
+
+public final class IconCatalog {
+
+    public static final List<Material> ICONS = List.of(
+            Material.GRASS_BLOCK,
+            Material.CHEST,
+            Material.ENCHANTING_TABLE,
+            Material.ROSE_BUSH,
+            Material.TURTLE_EGG,
+            Material.VERDANT_FROGLIGHT,
+            Material.HONEY_BLOCK,
+            Material.DIAMOND_PICKAXE,
+            Material.DIAMOND_AXE,
+            Material.SEA_LANTERN,
+            Material.TURTLE_HELMET,
+            Material.FURNACE,
+            Material.SMITHING_TABLE,
+            Material.ANVIL,
+            Material.BEACON,
+            Material.BEE_NEST,
+            Material.SAND,
+            Material.OAK_LOG,
+            Material.DIAMOND_ORE,
+            Material.DEEPSLATE_GOLD_ORE,
+            Material.GOLD_BLOCK,
+            Material.NETHERITE_BLOCK,
+            Material.DIAMOND_BLOCK,
+            Material.SMOOTH_QUARTZ,
+            Material.AMETHYST_BLOCK,
+            Material.PALE_OAK_LOG,
+            Material.POINTED_DRIPSTONE
+    );
+
+    private IconCatalog() {
+    }
+}
