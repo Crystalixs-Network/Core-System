abc679ef882e4843bb575da5a720c8f2a5ae9a90
Merry <svgr1808@outlook.de>
2026-03-26 13:50:11 +0100

    refactor(home-gui): resize gui

--- paper/src/main/java/net/crystalixs/core/paper/home/gui/HomeEditGui.java
+++ paper/src/main/java/net/crystalixs/core/paper/home/gui/HomeEditGui.java
@@ -31,7 +31,9 @@ public final class HomeEditGui implements HomeGui {
                 .setStructure(
                         "x x x x i x x x x",
                         "x x x x x x x x x",
-                        "x x x x b x x x x"
+                        "x x x x x x x x x",
+                        "x x x x x x x x x",
+                        "b x x x x x x x x"
                 )
                 .addIngredient('x', new SimpleItem(itemFactory.locked()))
                 .addIngredient('i', new SimpleItem(itemFactory.icon(model)))
