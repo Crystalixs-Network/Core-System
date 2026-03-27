198e066e5721e0e0ff881f568e88294438e2637e
Merry <svgr1808@outlook.de>
2026-03-27 21:57:08 +0100

    feat(ignore): bootstrap unignore command

--- paper/src/main/java/net/crystalixs/core/paper/bootstrap/PaperCommandBootstrap.java
+++ paper/src/main/java/net/crystalixs/core/paper/bootstrap/PaperCommandBootstrap.java
@@ -89,6 +89,7 @@ public final class PaperCommandBootstrap {
         new VanishCommand(plugin, vanishService).registerTo(commandManager);
         new HomeCommand(plugin, homeService, homeGuiFactory).registerTo(commandManager);
         new IgnoreCommand(plugin, settingService).registerTo(commandManager);
+        new UnignoreCommand(plugin, settingService).registerTo(commandManager);
 
         StructuredLogger helpSyncLogger = runtime.componentLogger("help-sync");
         String redisUri = configUpdater.current().redisSync() == null ? null : configUpdater.current().redisSync().uri();
