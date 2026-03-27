c3ed93e49271851b120b8e3387b4ba44d57436ca
Merry <svgr1808@outlook.de>
2026-03-27 21:34:15 +0100

    feat(ignore): bootstrap ignore command
    [CR-93]

--- paper/src/main/java/net/crystalixs/core/paper/bootstrap/PaperCommandBootstrap.java
+++ paper/src/main/java/net/crystalixs/core/paper/bootstrap/PaperCommandBootstrap.java
@@ -88,6 +88,7 @@ public final class PaperCommandBootstrap {
         new InventorySeeCommand(plugin, inventorySeeService).registerTo(commandManager);
         new VanishCommand(plugin, vanishService).registerTo(commandManager);
         new HomeCommand(plugin, homeService, homeGuiFactory).registerTo(commandManager);
+        new IgnoreCommand(plugin, settingService).registerTo(commandManager);
 
         StructuredLogger helpSyncLogger = runtime.componentLogger("help-sync");
         String redisUri = configUpdater.current().redisSync() == null ? null : configUpdater.current().redisSync().uri();
