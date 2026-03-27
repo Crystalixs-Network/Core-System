62fbfd1cda2c7d1d35dadddfe1fbdce517659d62
Merry <svgr1808@outlook.de>
2026-03-27 20:18:30 +0100

    feat(persistence): add player_setting table with migration to v3.0

---
+++ persistence/src/main/resources/database/mariadb/3/migrate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS player_setting
+(
+    id          BIGINT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    player_id   UUID    NOT NULL,
+    is_ignored  BOOLEAN NOT NULL DEFAULT FALSE,
+    is_vanished BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT fk_player_setting_player FOREIGN KEY (player_id) REFERENCES player (uuid) ON DELETE CASCADE
+);
+
+CREATE TRIGGER create_player_settings
+    AFTER INSERT
+    ON player
+    FOR EACH ROW
+    INSERT INTO player_setting (player_id)
+    VALUES (NEW.uuid);
