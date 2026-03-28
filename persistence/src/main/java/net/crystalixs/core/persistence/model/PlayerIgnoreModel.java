12c5c07f5b613d7902c1f0760cad716611177d8c
Merry <svgr1808@outlook.de>
2026-03-28 16:19:51 +0100

    feat(persistence): add row mapping to PlayerIgnoreModel
    [CR-96]

--- persistence/src/main/java/net/crystalixs/core/persistence/model/PlayerIgnoreModel.java
+++ persistence/src/main/java/net/crystalixs/core/persistence/model/PlayerIgnoreModel.java
@@ -1,7 +1,11 @@
 package net.crystalixs.core.persistence.model;
 
+import de.chojo.sadu.mapper.rowmapper.RowMapping;
+
 import java.time.Instant;
 import java.util.UUID;
+
+import static net.crystalixs.core.persistence.model.UuidReader.uuidReader;
 
 public record PlayerIgnoreModel(
         long id,
@@ -9,4 +13,13 @@ public record PlayerIgnoreModel(
         UUID ignoredUuid,
         Instant createdAt
 ) {
+
+    public static RowMapping<PlayerIgnoreModel> map() {
+        return row -> new PlayerIgnoreModel(
+                row.getLong("id"),
+                row.get("player_uuid", uuidReader()),
+                row.get("ignored_uuid", uuidReader()),
+                row.getTimestamp("created_at").toInstant());
+    }
+
 }
