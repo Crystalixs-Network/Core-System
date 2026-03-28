5fffed17b17c552db1456699324b786fcd152741
Merry <svgr1808@outlook.de>
2026-03-28 20:21:39 +0100

    refactor(ignore): rename `isIgnoredByPlayer` to `isIgnoring` across codebase for clarity
    Refers to: CR-95

--- persistence/src/main/java/net/crystalixs/core/persistence/defaults/DefaultPlayerIgnoreStore.java
+++ persistence/src/main/java/net/crystalixs/core/persistence/defaults/DefaultPlayerIgnoreStore.java
@@ -4,13 +4,10 @@ import de.chojo.sadu.queries.api.configuration.QueryConfiguration;
 import net.crystalixs.core.common.logging.LogMetadata;
 import net.crystalixs.core.common.logging.StructuredLogger;
 import net.crystalixs.core.persistence.PersistenceException;
-import net.crystalixs.core.persistence.model.PlayerIgnoreModel;
 import net.crystalixs.core.persistence.store.PlayerIgnoreStore;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.UnmodifiableView;
 
 import javax.sql.DataSource;
-import java.util.Collection;
 import java.util.UUID;
 
 import static de.chojo.sadu.queries.api.call.Call.call;
@@ -25,18 +22,6 @@ public final class DefaultPlayerIgnoreStore implements PlayerIgnoreStore {
         this.config = QueryConfiguration.builder(dataSource)
                 .setThrowExceptions(true)
                 .build();
-    }
-
-    @Override
-    public @NotNull @UnmodifiableView Collection<PlayerIgnoreModel> findByPlayer(@NotNull UUID playerUuid) {
-        try {
-            return config.query("SELECT * FROM player_ignore WHERE player_uuid = ?;")
-                    .single(call().bind(playerUuid.toString()))
-                    .map(PlayerIgnoreModel.map())
-                    .all();
-        } catch (RuntimeException exception) {
-            throw failure("persistence.player_ignore.find_failed", "Could not load ignored players", exception, playerUuid);
-        }
     }
 
     @Override
