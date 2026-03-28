422ddcf32de2554521a3095d4787f52c873114f7
Merry <svgr1808@outlook.de>
2026-03-28 20:55:47 +0100

    feat(ignore): implement unignore command
    Refers to: CR-94

--- paper/src/main/java/net/crystalixs/core/paper/command/UnignoreCommand.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/UnignoreCommand.java
@@ -5,11 +5,14 @@ import net.crystalixs.core.paper.command.cloud.PaperCommand;
 import net.crystalixs.core.paper.command.cloud.PaperCommandSource;
 import net.crystalixs.core.paper.command.cloud.PaperPlayerCommandSource;
 import net.crystalixs.core.paper.ignore.PlayerIgnoreService;
+import org.bukkit.entity.Player;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.minecraft.extras.RichDescription;
 import org.incendo.cloud.permission.Permission;
 import org.jetbrains.annotations.NotNull;
 
+import static net.kyori.adventure.text.Component.translatable;
+import static net.kyori.adventure.text.minimessage.translation.Argument.component;
 import static org.incendo.cloud.bukkit.parser.PlayerParser.playerParser;
 
 public final class UnignoreCommand extends PaperCommand {
@@ -29,7 +32,20 @@ public final class UnignoreCommand extends PaperCommand {
                 .permission(Permission.of("core.command.unignore"))
                 .required("player", playerParser())
                 .handler(context -> {
+                    Player sender = context.sender().player();
+                    Player target = context.get("player");
 
+                    if (sender.getUniqueId().equals(target.getUniqueId())) {
+                        sender.sendMessage(translatable("command.unignore.error.self"));
+                        return;
+                    }
+                    if (!service.isIgnoring(sender, target)) {
+                        sender.sendMessage(translatable("command.unignore.error.not-ignored"));
+                        return;
+                    }
+
+                    service.unignorePlayer(sender, target);
+                    sender.sendMessage(translatable("command.unignore.success").arguments(component("name", target.name())));
                 }));
     }
 }
