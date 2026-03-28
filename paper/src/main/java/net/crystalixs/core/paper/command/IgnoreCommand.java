32b2ba466bff33090800bc0d8dbcfcba6e03def3
Merry <svgr1808@outlook.de>
2026-03-28 17:50:31 +0100

    feat(ignore): implement ignore command with happy path
    Refers to: CR-93

--- paper/src/main/java/net/crystalixs/core/paper/command/IgnoreCommand.java
+++ paper/src/main/java/net/crystalixs/core/paper/command/IgnoreCommand.java
@@ -3,9 +3,15 @@ package net.crystalixs.core.paper.command;
 import net.crystalixs.core.paper.CorePlugin;
 import net.crystalixs.core.paper.command.cloud.PaperCommand;
 import net.crystalixs.core.paper.command.cloud.PaperCommandSource;
+import net.crystalixs.core.paper.command.cloud.PaperPlayerCommandSource;
 import net.crystalixs.core.paper.ignore.PlayerIgnoreService;
+import org.bukkit.entity.Player;
 import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.minecraft.extras.RichDescription;
+import org.incendo.cloud.permission.Permission;
 import org.jetbrains.annotations.NotNull;
+
+import static org.incendo.cloud.bukkit.parser.PlayerParser.playerParser;
 
 public final class IgnoreCommand extends PaperCommand {
 
@@ -18,5 +24,16 @@ public final class IgnoreCommand extends PaperCommand {
 
     @Override
     public void registerTo(@NotNull CommandManager<PaperCommandSource> commandManager) {
+        commandManager.command(commandManager.commandBuilder("ignore")
+                .commandDescription(RichDescription.translatable("command.ignore.description.main"))
+                .senderType(PaperPlayerCommandSource.class)
+                .permission(Permission.of("core.command.ignore"))
+                .required("player", playerParser())
+                .handler(context -> {
+                    Player sender = context.sender().player();
+                    Player target = context.get("player");
+
+                    service.ignorePlayer(sender, target);
+                }));
     }
 }
