8694f7d696e9d890ab52009ab2acbe0058d2027b
Merry <svgr1808@outlook.de>
2026-03-31 14:01:53 +0200

    refactor(core): enforce strict nomenclature of pattern `core-*`
    Refers to: CR-137
    Time-Spent: 1m

---
+++ core-persistence/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `java-library`
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    api(project(":core-common"))
+    implementation(libs.mariadb)
+    implementation(libs.bundles.sadu)
+}
+
+tasks {
+    val artifact = project.mavenArtifact()
+
+    shadowJar {
+        val mapping = mapOf(
+            libs.sadu.datasource to "sadu.datasource",
+            libs.sadu.mariadb to "sadu.mariadb",
+            libs.sadu.queries to "sadu.queries",
+            libs.sadu.updater to "sadu.updater",
+            libs.mariadb to "mariadb",
+        )
+
+        val base = "$group.$artifact.persistence.libs"
+        for ((dependency, name) in mapping) relocate(dependency.get().group, "$base.$name")
+    }
+
+    jar {
+        archiveBaseName.set("$artifact-persistence")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
