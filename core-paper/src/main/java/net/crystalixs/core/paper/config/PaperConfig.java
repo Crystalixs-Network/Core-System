5f177f79eb3da69dca164dcb0e82cd50c0b28959
Merry <svgr1808@outlook.de>
2026-04-02 12:25:35 +0200

    feat(paper): make hot reloading config option configurable
    Refers to: CR-127
    Time-Spent: 3m

--- core-paper/src/main/java/net/crystalixs/core/paper/config/PaperConfig.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/config/PaperConfig.java
@@ -1,7 +1,13 @@
 package net.crystalixs.core.paper.config;
 
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 @ConfigSerializable
-public record PaperConfig(Database database, RedisSync redisSync, Scoreboard scoreboard) {
+public record PaperConfig(
+        @Setting("enable-hot-reloading") boolean isHotReloadingEnabled,
+        Database database,
+        RedisSync redisSync,
+        Scoreboard scoreboard
+) {
 }
