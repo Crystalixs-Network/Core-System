79069e021636c4cd5b0815913495321b23258333
Merry <svgr1808@outlook.de>
2026-04-02 20:23:04 +0200

    refactor: apply method signature change for ScoreboardPlaceholderResolver
    Refers to: CR-129
    Time-Spent: 2m

--- core-paper/src/main/java/net/crystalixs/core/paper/scoreboard/ScoreboardService.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/scoreboard/ScoreboardService.java
@@ -32,7 +32,7 @@ public final class ScoreboardService {
     private final StructuredLogger logger;
     private final PaperConfig config;
     private final LuckPerms luckPerms;
-    private final ScoreboardPlaceholderResolver<Player> rankResolver;
+    private final ScoreboardPlaceholderResolver<Component> rankResolver;
     private EventSubscription<UserDataRecalculateEvent> subscription;
 
     private ScoreboardService(JavaPlugin plugin, StructuredLogger logger, PaperConfig config) {
