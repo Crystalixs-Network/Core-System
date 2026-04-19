54390a5349e1c3a8dd38aec3a326aee0008187df
Merry <svgr1808@outlook.de>
2026-04-19 17:33:15 +0200

    feat: implement drop multiplier by level
    Refers to: CR-213
    Time-Spent: 1m

--- core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/WoodWhisperEnchantmentHandler.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/WoodWhisperEnchantmentHandler.java
@@ -2,6 +2,11 @@ package net.crystalixs.core.paper.enchantment.impl;
 
 import net.crystalixs.core.paper.enchantment.BreakingBlocksEnchantmentContext;
 import net.crystalixs.core.paper.enchantment.CustomEnchantmentHandler;
+import org.bukkit.Tag;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
 
 public final class WoodWhisperEnchantmentHandler implements CustomEnchantmentHandler {
 
@@ -12,5 +17,26 @@ public final class WoodWhisperEnchantmentHandler implements CustomEnchantmentHan
 
     @Override
     public void handle(BreakingBlocksEnchantmentContext context, int level) {
+        Block block = context.block();
+
+        if (!Tag.ITEMS_AXES.isTagged(context.tool().getType())) return;
+        if (!Tag.LOGS.isTagged(block.getType())) return;
+
+        int multiplier = multiplier(level);
+        Collection<ItemStack> drops = block.getDrops(context.tool(), context.player());
+
+        for (int i = 0; i < multiplier; i++) {
+            for (ItemStack drop : drops) {
+                block.getWorld().dropItemNaturally(block.getLocation(), drop.clone());
+            }
+        }
+    }
+
+    private int multiplier(int level) {
+        return switch (level) {
+            case 1 -> 3;
+            case 2 -> 5;
+            default -> 7;
+        };
     }
 }
