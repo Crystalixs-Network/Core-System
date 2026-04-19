fdead0b62ab815b3c4b816bb372b32becc7fb87d
Merry <svgr1808@outlook.de>
2026-04-19 17:47:31 +0200

    refactor: wire leaf mining enchantment with registry
    Refers to: CR-213
    Time-Spent: 1m

--- core-paper/src/main/java/net/crystalixs/core/paper/bootstrap/PaperCustomEnchantmentBootstrap.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/bootstrap/PaperCustomEnchantmentBootstrap.java
@@ -1,6 +1,7 @@
 package net.crystalixs.core.paper.bootstrap;
 
 import net.crystalixs.core.paper.enchantment.CustomEnchantmentRegistry;
+import net.crystalixs.core.paper.enchantment.impl.LeafMiningEnchantmentHandler;
 import net.crystalixs.core.paper.enchantment.impl.TimberEnchantmentHandler;
 import net.crystalixs.core.paper.enchantment.impl.WoodWhisperEnchantmentHandler;
 
@@ -11,7 +12,8 @@ public final class PaperCustomEnchantmentBootstrap {
     public CustomEnchantmentRegistry createRegistry() {
         return new CustomEnchantmentRegistry(List.of(
                 new TimberEnchantmentHandler(),
-                new WoodWhisperEnchantmentHandler()
+                new WoodWhisperEnchantmentHandler(),
+                new LeafMiningEnchantmentHandler()
         ));
     }
 }
