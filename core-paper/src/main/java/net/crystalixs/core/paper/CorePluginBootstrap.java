c12724a68ccae5f78f49c953a007c71408a75af9
Merry <svgr1808@outlook.de>
2026-04-21 14:25:41 +0200

    chore(bootstrap): scaffold paper plugin bootstrap for registry setup
    Refers to: CR-213
    Time-Spent: 1m

---
+++ core-paper/src/main/java/net/crystalixs/core/paper/CorePluginBootstrap.java
@@ -0,0 +1,12 @@
+package net.crystalixs.core.paper;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import org.jetbrains.annotations.NotNull;
+
+public final class CorePluginBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(@NotNull BootstrapContext context) {
+    }
+}
