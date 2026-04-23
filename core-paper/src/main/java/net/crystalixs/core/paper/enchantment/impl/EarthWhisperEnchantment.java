63aa6b5fa17928cf8d032f001e843c1adadd3a3c
Merry <svgr1808@outlook.de>
2026-04-23 13:54:55 +0200

    feat(enchantments): scaffold earth whisper and plow enchantments
    Refers to: CR-215
    Time-Spent: 2m

---
+++ core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/EarthWhisperEnchantment.java
@@ -0,0 +1,11 @@
+package net.crystalixs.core.paper.enchantment.impl;
+
+import net.crystalixs.core.paper.enchantment.CustomEnchantment;
+
+public final class EarthWhisperEnchantment implements CustomEnchantment {
+
+    @Override
+    public String id() {
+        return "earth_whisper";
+    }
+}
