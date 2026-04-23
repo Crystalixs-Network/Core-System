3e149073aa6fb1e764149e47102b308dfea6d253
Merry <svgr1808@outlook.de>
2026-04-23 17:34:50 +0200

    feat(collector): implement basic block break behavior
    Refers to: CR-216
    Time-Spent: 1m

--- core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/ResourceCollectorEnchantment.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/ResourceCollectorEnchantment.java
@@ -1,11 +1,35 @@
 package net.crystalixs.core.paper.enchantment.impl;
 
 import net.crystalixs.core.paper.enchantment.CustomEnchantment;
+import net.crystalixs.core.paper.enchantment.EnchantmentContext;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
+import java.util.Map;
 
 public final class ResourceCollectorEnchantment implements CustomEnchantment {
 
     @Override
     public String id() {
         return "resource_collector";
+    }
+
+    @Override
+    public void onBlockBreak(EnchantmentContext.BreakingBlocksContext context, int level) {
+        Collection<ItemStack> drops = context.block().getDrops(context.tool(), context.player());
+        if (drops.isEmpty()) return;
+
+        context.event().setDropItems(false);
+        giveOrDropNaturally(context.player(), context.block().getLocation(), drops);
+    }
+
+    private void giveOrDropNaturally(Player player, Location location, Collection<ItemStack> drops) {
+        Map<Integer, ItemStack> leftovers = player.getInventory().addItem(drops.toArray(ItemStack[]::new));
+        leftovers.values().forEach(itemStack -> {
+            if (itemStack == null || itemStack.getType().isAir()) return;
+            location.getWorld().dropItemNaturally(location, itemStack);
+        });
     }
 }
