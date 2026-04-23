ceaf7069efd9cec12048cd63ab6c1e0c2073d7f2
Merry <svgr1808@outlook.de>
2026-04-23 12:22:19 +0200

    feat(vein-mining): implement basic vein mining behavior
    Refers to: CR-214
    Time-Spent: 1m

--- core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/VeinMiningEnchantment.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/VeinMiningEnchantment.java
@@ -1,11 +1,48 @@
 package net.crystalixs.core.paper.enchantment.impl;
 
+import net.crystalixs.core.paper.enchantment.BreakingBlocksEnchantmentContext;
 import net.crystalixs.core.paper.enchantment.CustomEnchantment;
+import net.crystalixs.core.paper.util.BlockFloodFill;
+import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.bukkit.block.Block;
 
-public final class VeinMiningEnchantment implements CustomEnchantment {
+import java.util.Set;
+
+public final class VeinMiningEnchantment implements CustomEnchantment, BlockFloodFill {
 
     @Override
     public String id() {
         return "vein_mining";
+    }
+
+    @Override
+    public void onBlockBreak(BreakingBlocksEnchantmentContext context, int level) {
+        Block origin = context.block();
+
+        if (!Tag.ITEMS_PICKAXES.isTagged(context.tool().getType())) return;
+        if (!matches(origin)) return;
+
+        Material oreType = origin.getType();
+        Set<Block> vein = fill(origin, cap(level));
+
+        for (Block block : vein) {
+            if (block.equals(origin)) continue;
+            if (block.getType() != oreType) continue;
+            block.breakNaturally(context.tool(), true);
+        }
+    }
+
+    private int cap(int level) {
+        return switch (Math.clamp(level, 1, 3)) {
+            case 1 -> 10;
+            case 2 -> 15;
+            default -> 24;
+        };
+    }
+
+    @Override
+    public boolean matches(Block block) {
+        return block.getType().name().endsWith("_ORE") || block.getType() == Material.ANCIENT_DEBRIS;
     }
 }
