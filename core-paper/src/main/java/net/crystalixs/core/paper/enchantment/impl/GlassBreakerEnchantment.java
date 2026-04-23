3ab16ed02db9c0a755e4f148fe248bac71755bd6
Merry <svgr1808@outlook.de>
2026-04-23 20:03:59 +0200

    feat(glass-breaker): implement basic behavior
    Refers to: CR-216
    Time-Spent: 1m

--- core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/GlassBreakerEnchantment.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/GlassBreakerEnchantment.java
@@ -1,11 +1,32 @@
 package net.crystalixs.core.paper.enchantment.impl;
 
 import net.crystalixs.core.paper.enchantment.CustomEnchantment;
+import net.crystalixs.core.paper.enchantment.EnchantmentContext;
+import org.bukkit.Material;
+import org.bukkit.Tag;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
 
 public final class GlassBreakerEnchantment implements CustomEnchantment {
 
     @Override
     public String id() {
         return "glass_breaker";
+    }
+
+    @Override
+    public void onBlockBreak(EnchantmentContext.BreakingBlocksContext context, int level) {
+        if (!Tag.ITEMS_PICKAXES.isTagged(context.tool().getType())) return;
+        if (!isGlass(context.block())) return;
+
+        context.block().getWorld().dropItemNaturally(
+                context.block().getLocation(),
+                new ItemStack(context.block().getType()));
+    }
+
+    private boolean isGlass(Block block) {
+        return block.getType().name().contains("GLASS")
+               && block.getType() != Material.SPYGLASS
+               && block.getType() != Material.GLASS_BOTTLE;
     }
 }
