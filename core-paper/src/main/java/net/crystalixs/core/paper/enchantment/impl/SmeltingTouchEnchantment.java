a540bfdabe5e66672f91cc9db0b41c1cd4d0608f
Merry <svgr1808@outlook.de>
2026-04-24 16:29:23 +0200

    feat(smelting-touch): scaffold block break hook
    Refers to: CR-216
    Time-Spent: 1m

--- core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/SmeltingTouchEnchantment.java
+++ core-paper/src/main/java/net/crystalixs/core/paper/enchantment/impl/SmeltingTouchEnchantment.java
@@ -1,11 +1,19 @@
 package net.crystalixs.core.paper.enchantment.impl;
 
 import net.crystalixs.core.paper.enchantment.CustomEnchantment;
+import net.crystalixs.core.paper.enchantment.EnchantmentContext;
+import org.bukkit.Tag;
 
 public final class SmeltingTouchEnchantment implements CustomEnchantment {
 
     @Override
     public String id() {
         return "smelting_touch";
+    }
+
+    @Override
+    public void onBlockBreak(EnchantmentContext.BreakingBlocksContext context, int level) {
+        if (!Tag.ITEMS_PICKAXES.isTagged(context.tool().getType())) return;
+        if (context.block().getDrops(context.tool(), context.player()).isEmpty()) return;
     }
 }
